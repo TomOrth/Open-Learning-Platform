d1f5f0b3d7c36c0c9576f2cab017747159230bda
Tom Orth <torth212@gmail.com>
2020-02-23 21:33:19 -0500

    Update initial prototype

---
+++ src/app/controllers/educators_controller.rb
@@ -0,0 +1,4 @@
+class EducatorsController < ApplicationController
+  def show
+  end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  resources :lesson_plans
-  devise_for :educators, path: 'educators'
-  devise_for :admins, path: 'admins'
+  resources :lesson_plans 
+  devise_for :educators, path: 'educators', controllers: {
+    passwords: 'educators/passwords',
+    registrations: 'educators/registrations',
+    sessions: 'educators/sessions'
+  }
+  devise_for :admins, path: 'admins', controllers: {
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions'
+  }
+
+  resources :educators
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  namespace :dashboard do
+     resources :admins
+  end
 end
